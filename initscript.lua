4a8509ebde255277b1612e78d2a7268c63883b22
xiaomasoftworks <xiaomatan3@gmail.com>
2025-04-21 11:27:35 +0700

    Update initscript.lua

--- initscript.lua
+++ initscript.lua
@@ -1,5 +1,36 @@
 if not game:IsLoaded() then game["Loaded"]:Wait() end
 
+setreadonly(getgenv().debug,false)
+getgenv().debug.traceback = getrenv().debug.traceback
+getgenv().debug.profilebegin = getrenv().debug.profilebegin
+getgenv().debug.profileend = getrenv().debug.profileend
+getgenv().debug.getmetatable = getgenv().getrawmetatable
+getgenv().debug.setmetatable = getgenv().setrawmetatable
+getgenv().debug.info = getrenv().debug.info
+getgenv().debug.loadmodule = getrenv().debug.loadmodule
+
+getgenv().syn_mouse1press = mouse1press
+getgenv().syn_mouse2click = mouse2click
+getgenv().syn_mousemoverel = movemouserel
+getgenv().syn_mouse2release = mouse2up
+getgenv().syn_mouse1release = mouse1up
+getgenv().syn_mouse2press = mouse2down
+getgenv().syn_mouse1click = mouse1click
+getgenv().syn_newcclosure = newcclosure
+getgenv().syn_clipboard_set = setclipboard
+getgenv().syn_clipboard_get = getclipboard
+getgenv().syn_islclosure = islclosure
+getgenv().syn_iscclosure = iscclosure
+getgenv().syn_getsenv = getsenv
+getgenv().syn_getscripts = getscripts
+getgenv().syn_getgenv = getgenv
+getgenv().syn_getinstances = getinstances
+getgenv().syn_getreg = getreg
+getgenv().syn_getrenv = getrenv
+getgenv().syn_getnilinstances = getnilinstances
+getgenv().syn_fireclickdetector = fireclickdetector
+getgenv().syn_getgc = getgc
+
 getgenv().saveinstance = newcclosure(function(Options)
 	 local Params = {
         RepoURL = "https://raw.githubusercontent.com/luau/SynSaveInstance/main/",
@@ -238,8 +269,6 @@ getgenv().getconnection = newcclosure(function(signal, index)
         return nil
     end
 end)
-
-load_autoexec_scripts()
 
 --loadstring(game:HttpGet('https://raw.githubusercontent.com/xiaomasoftworks/ElephatScripts/refs/heads/main/luaudrawlib.lua'))()
 

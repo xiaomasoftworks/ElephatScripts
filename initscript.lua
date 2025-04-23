bb6a21085979f03d935f411f28ae78e60fee3a54
xiaomasoftworks <xiaomatan3@gmail.com>
2025-04-23 13:18:32 +0700

    Update initscript.lua

--- initscript.lua
+++ initscript.lua
@@ -1,3 +1,5 @@
+-- fuck you zynox dont stole ts pls
+
 if not game:IsLoaded() then game["Loaded"]:Wait() end
 
 setreadonly(getgenv().debug,false)
@@ -8,28 +10,6 @@ getgenv().debug.getmetatable = getgenv().getrawmetatable
 getgenv().debug.setmetatable = getgenv().setrawmetatable
 getgenv().debug.info = getrenv().debug.info
 getgenv().debug.loadmodule = getrenv().debug.loadmodule
-
-getgenv().syn_mouse1press = mouse1press
-getgenv().syn_mouse2click = mouse2click
-getgenv().syn_mousemoverel = movemouserel
-getgenv().syn_mouse2release = mouse2up
-getgenv().syn_mouse1release = mouse1up
-getgenv().syn_mouse2press = mouse2down
-getgenv().syn_mouse1click = mouse1click
-getgenv().syn_newcclosure = newcclosure
-getgenv().syn_clipboard_set = setclipboard
-getgenv().syn_clipboard_get = getclipboard
-getgenv().syn_islclosure = islclosure
-getgenv().syn_iscclosure = iscclosure
-getgenv().syn_getsenv = getsenv
-getgenv().syn_getscripts = getscripts
-getgenv().syn_getgenv = getgenv
-getgenv().syn_getinstances = getinstances
-getgenv().syn_getreg = getreg
-getgenv().syn_getrenv = getrenv
-getgenv().syn_getnilinstances = getnilinstances
-getgenv().syn_fireclickdetector = fireclickdetector
-getgenv().syn_getgc = getgc
 
 getgenv().saveinstance = newcclosure(function(Options)
 	 local Params = {
@@ -285,6 +265,29 @@ getgenv().getconnection = newcclosure(function(signal, index)
         return nil
     end
 end)
+
+getgenv().syn_mouse1press = mouse1press
+getgenv().syn_mouse2click = mouse2click
+getgenv().syn_mousemoverel = movemouserel
+getgenv().syn_mouse2release = mouse2up
+getgenv().syn_mouse1release = mouse1up
+getgenv().syn_mouse2press = mouse2down
+getgenv().syn_mouse1click = mouse1click
+getgenv().syn_newcclosure = newcclosure
+getgenv().syn_clipboard_set = setclipboard
+getgenv().syn_clipboard_get = getclipboard
+getgenv().syn_islclosure = islclosure
+getgenv().syn_iscclosure = iscclosure
+getgenv().syn_getsenv = getsenv
+getgenv().syn_getscripts = getscripts
+getgenv().syn_getgenv = getgenv
+getgenv().syn_getinstances = getinstances
+getgenv().syn_getreg = getreg
+getgenv().syn_getrenv = getrenv
+getgenv().syn_getnilinstances = getnilinstances
+getgenv().syn_fireclickdetector = fireclickdetector
+getgenv().syn_getgc = getgc
+
 
 --loadstring(game:HttpGet('https://raw.githubusercontent.com/xiaomasoftworks/ElephatScripts/refs/heads/main/luaudrawlib.lua'))()
 

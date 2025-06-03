78f1a8696719420e1f14714a1ab8bdfb481314d1
xiaomasoftworks <xiaomatan3@gmail.com>
2025-06-03 19:08:15 +0700

    Create initx.lua

---
+++ initx.lua
@@ -0,0 +1,122 @@
+if not game:IsLoaded() then game["Loaded"]:Wait() end
+
+setreadonly(getgenv().debug,false)
+getgenv().debug.traceback = getrenv().debug.traceback
+getgenv().debug.profilebegin = getrenv().debug.profilebegin
+getgenv().debug.profileend = getrenv().debug.profileend
+getgenv().debug.getmetatable = getgenv().getrawmetatable
+getgenv().debug.setmetatable = getgenv().setrawmetatable
+getgenv().debug.info = getrenv().debug.info
+getgenv().debug.loadmodule = getrenv().debug.loadmodule
+
+getgenv().saveinstance = newcclosure(function(Options)
+	 local Params = {
+        RepoURL = "https://raw.githubusercontent.com/luau/SynSaveInstance/main/",
+        SSI = "saveinstance",
+     }
+     local synsaveinstance = loadstring(game:HttpGet(Params.RepoURL .. Params.SSI .. ".luau", true), Params.SSI)()
+     --local Options = {} -- Documentation here https://luau.github.io/UniversalSynSaveInstance/api/SynSaveInstance
+     synsaveinstance(Options)
+end)
+
+local cache = {}
+getgenv().getsenv = newcclosure(function(scr)
+    
+    if cache[scr] then
+        return cache[scr]
+    end
+
+	for i, v in getreg() do
+		if type(v) == "thread" then
+			local tenv = gettenv(v);
+			if tenv.script == scr then
+				cache[scr] = tenv
+				return tenv;
+			end
+		end
+	end
+end)
+
+getgenv()["savegame"] = saveinstance
+getgenv()["saveplace"] = saveinstance
+
+getgenv()["lz4_compress"] = lz4compress
+getgenv()["Lz4Compress"] = lz4compress
+
+getgenv()["lz4_decompress"] = lz4decompress
+getgenv()["Lz4Decompress"] = lz4decompress
+
+
+local function RobloxConsoleHandler(...)
+	warn("rconsole library disabled.")
+end
+
+getgenv()["rconsoleclear"] = newcclosure(RobloxConsoleHandler)
+getgenv()["consoleclear"] = rconsoleclear
+getgenv()["console_clear"] = rconsoleclear
+getgenv()["ConsoleClear"] = rconsoleclear
+
+getgenv()["rconsolecreate"] = newcclosure(RobloxConsoleHandler)
+getgenv()["consolecreate"] = rconsolecreate
+getgenv()["console_create"] = rconsolecreate
+getgenv()["ConsoleCreate"] = rconsolecreate
+
+getgenv()["rconsoledestroy"] = newcclosure(RobloxConsoleHandler)
+getgenv()["consoledestroy"] = rconsoledestroy
+getgenv()["console_destroy"] = rconsoledestroy
+getgenv()["ConsoleDestroy"] = rconsoledestroy
+
+getgenv()["rconsoleinput"] = newcclosure(RobloxConsoleHandler)
+getgenv()["consoleinput"] = rconsoleinput
+getgenv()["console_input"] = rconsoleinput
+getgenv()["ConsoleInput"] = rconsoleinput
+
+getgenv()["rconsoleprint"] = newcclosure(RobloxConsoleHandler)
+getgenv()["consoleprint"] = rconsoleprint
+getgenv()["console_print"] = rconsoleprint
+getgenv()["ConsolePrint"] = rconsoleprint
+
+getgenv()["rconsolesettitle"] = newcclosure(RobloxConsoleHandler)
+getgenv()["rconsolename"] = rconsolesettitle
+getgenv()["consolesettitle"] = rconsolesettitle
+getgenv()["console_set_title"] = rconsolesettitle
+getgenv()["ConsoleSetTitle"] = rconsolesettitle
+
+getgenv().http = {}
+getgenv().http.request = request
+setreadonly(http, true)
+
+getgenv().http_request = request
+
+getgenv().getconnection = newcclosure(function(signal, index)
+    local connections = getconnections(signal)
+    if index > 0 and index <= #connections then
+        return connections[index]
+    else
+        return nil
+    end
+end)
+
+getgenv().syn_mouse1press = mouse1press
+getgenv().syn_mouse2click = mouse2click
+getgenv().syn_mousemoverel = movemouserel
+getgenv().syn_mouse2release = mouse2up
+getgenv().syn_mouse1release = mouse1up
+getgenv().syn_mouse2press = mouse2down
+getgenv().syn_mouse1click = mouse1click
+getgenv().syn_newcclosure = newcclosure
+getgenv().syn_clipboard_set = setclipboard
+getgenv().syn_clipboard_get = getclipboard
+getgenv().syn_islclosure = islclosure
+getgenv().syn_iscclosure = iscclosure
+getgenv().syn_getsenv = getsenv
+getgenv().syn_getscripts = getscripts
+getgenv().syn_getgenv = getgenv
+getgenv().syn_getinstances = getinstances
+getgenv().syn_getreg = getreg
+getgenv().syn_getrenv = getrenv
+getgenv().syn_getnilinstances = getnilinstances
+getgenv().syn_fireclickdetector = fireclickdetector
+getgenv().syn_getgc = getgc
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xiaomasoftworks/ElephatScripts/refs/heads/main/luaudrawlib.lua'))()
